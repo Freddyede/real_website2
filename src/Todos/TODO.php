5da0d2f22fcf4cce16f99ffabb960c037cca361a
Freddyede <patouillardfranck3@gmail.com>
2020-01-30 03:32:59 +0100

    first commit

---
+++ src/Todos/TODO.php
@@ -0,0 +1,11 @@
+<?php
+
+#// INFORMATIONS : Visited this link for trello : https://trello.com/b/1N8bZNDJ/website2
+#// INFORMATIONS : Visited this link for github : 
+
+
+#// TODO: Edit Security and Guard for route AdminController (config/packages/security.yaml)
+#// TODO: Create HomeController and AdminController (src/HomeController.php)_______________
+
+#// DONE: Created database for dev website and server website_ (./.env)
+#// DONE: Created Entity User with them security (src/Entity/User.php)_
