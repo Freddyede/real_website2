cb7057ec92254c046a9cff34f6fe7ff38af64187
Freddyede <patouillardfranck3@gmail.com>
2020-02-03 12:11:05 +0100

    Add navbar.html.twig to develop

--- src/Controller/FilmsController.php
+++ src/Controller/FilmsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Acteurs;
 use App\Entity\Films;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FilmsController extends AbstractController
@@ -21,6 +22,19 @@ class FilmsController extends AbstractController
         }
         return $this->render('films/index.html.twig', array(
             'films'=>$this->getDoctrine()->getRepository(Films::class)->findBy(array('titre'=>$titre_acteurs)),
+            'controller_name' => 'FilmsController',
+        ));
+    }
+
+    /**
+     * @Route("/film/{id}", name="findFilm")
+     * @param $id
+     * @return Response
+     */
+    public function find($id)
+    {
+        return $this->render('films/read.html.twig', array(
+            'film'=>$this->getDoctrine()->getRepository(Films::class)->find($id),
             'controller_name' => 'FilmsController',
         ));
     }
